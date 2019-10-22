3ac1ce3f7bbeae6a3f2ad612e0a2987775ec4714
Kamila Sarsam <kmsarsam@gmail.com>
2019-10-22 16:02:52 -0700

    Python Django Website Added

---
+++ ContnetManagementSystem/ContnetManagementSystem/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kmsar\source\CIS440_Group4\ContnetManagementSystem\ContnetManagementSystem\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
