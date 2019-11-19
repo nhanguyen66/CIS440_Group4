0d8e8cefaccca27250277327e3e64554b82bbb68
Kamila Sarsam <kmsarsam@gmail.com>
2019-11-19 14:55:05 -0700

    fixed textfield, adding sample input for testing

--- ContentManagmentSystem/ContentManagmentSystem/app/forms.py
+++ ContentManagmentSystem/ContentManagmentSystem/app/forms.py
@@ -1,5 +1,27 @@
 """
 Definition of forms.
+
+SAMPLE TEXTS:
+
+short samples:
+Discover how well we can take care of you today. Members who switched saved an average of $707 a year
+
+paragraph:
+The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.
+
+with new lines:
+Protection for You and Your Passengers
+Depending on where you live, how much you drive and what your health insurance covers, the amount of protection you need for each of these can vary:
+
+Personal Injury Protection 
+Extended Benefits Coverage
+Uninsured and Underinsured Motorist Bodily Injury Coverage
+Uninsured and Underinsured Motorist Property Damage Coverage
+Note: The amount you choose for these types of car insurance coverage must be the same for each vehicle on your policy.
+
+Roadside Assistance
+Pays toward the cost to tow or repair your vehicle if you are stranded and your vehicle won't run. It also includes services to unlock your vehicle, deliver gas or change a tire.
+
 """
 
 from django import forms
@@ -22,13 +44,10 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                 'class': 'form-control',
                                 'placeholder':'Type or paste your text'}))
 
-#class ProcessText(forms.Form):
-#    """Authentication form which uses boostrap CSS."""
-#    text_input = forms.Textarea(max_lenth=900, required = True, help_text = "enter text")
 
 class TextEditorForm(forms.ModelForm):
     textinput=forms.CharField(label=_("Text Input"),
-                            widget=forms.TextInput ({
+                            widget=forms.Textarea ({
                                 'class': 'form-control',
                                 'placeholder':'Type or paste your text'}))
     class Meta:
--- ContentManagmentSystem/ContentManagmentSystem/app/models.py
+++ ContentManagmentSystem/ContentManagmentSystem/app/models.py
@@ -2,6 +2,9 @@
 Definition of models.
 """
 
+"""
+Aoccdrnig to a rscheearch at Cmabrigde Uinervtisy, it deosn’t mttaer in waht oredr the ltteers in a wrod are, the olny iprmoetnt tihng is taht the frist and lsat ltteer be at the rghit pclae. The rset can be a total mses and you can sitll raed it wouthit a porbelm. Tihs is bcuseae the huamn mnid deos not raed ervey lteter by istlef, but the wrod as a wlohe. Amzanig huh?
+"""
 from django.db import models
 import cgi, cgitb
 from spellchecker import SpellChecker
@@ -10,7 +13,7 @@ import cgi, cgitb
 # Create your models here.
 
 class TextEditor(models.Model):
-    textinput = models.TextField(max_length=500)
+    textinput = models.TextField(blank=True)
     textoutput = "sample output"
 
     def __str__(self):
@@ -22,7 +25,7 @@ class TextEditor(models.Model):
     def check_spelling(self):
         split_message = self.textoutput.split()
         spell = SpellChecker()
-        
+
         self.corrected_string = []
         for word in split_message:
             self.corrected_string.append(spell.correction(word))
