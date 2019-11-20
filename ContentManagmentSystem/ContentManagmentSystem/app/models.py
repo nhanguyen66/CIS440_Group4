6a83e33ce3d2d66a873c6549abe910587b59e62b
Kamila Sarsam <kmsarsam@gmail.com>
2019-11-19 22:17:11 -0700

    added reading difficulty and possible word replacements

--- ContentManagmentSystem/ContentManagmentSystem/app/models.py
+++ ContentManagmentSystem/ContentManagmentSystem/app/models.py
@@ -2,20 +2,22 @@
 Definition of models.
 """
 
-"""
-Aoccdrnig to a rscheearch at Cmabrigde Uinervtisy, it deosn’t mttaer in waht oredr the ltteers in a wrod are, the olny iprmoetnt tihng is taht the frist and lsat ltteer be at the rghit pclae. The rset can be a total mses and you can sitll raed it wouthit a porbelm. Tihs is bcuseae the huamn mnid deos not raed ervey lteter by istlef, but the wrod as a wlohe. Amzanig huh?
-"""
 from django.db import models
 import cgi, cgitb
 from spellchecker import SpellChecker
 import cgi, cgitb
 import nltk
 nltk.download('stopwords')
+nltk.download('wordnet')
 from nltk.corpus import stopwords
+from nltk.corpus import wordnet
 from nltk.cluster.util import cosine_distance
 import numpy as np
 import networkx as nx
 import language_check as lc
+import textstat
+import requests
+import re
 
 # Create your models here.
 
@@ -58,7 +60,7 @@ class TextEditor(models.Model):
     def process_text(self):
         #self.textoutput = self.textinput + "!"
         self.textoutput = self.testinput + "!"
-        self.textoutput = self.textoutput.replace("Discver","Discover",1)
+
 
     #replaces mispelled words
     def check_spelling(self):
@@ -73,61 +75,101 @@ class TextEditor(models.Model):
 
         for word in stringsToCorrect:
             self.textoutput = self.textoutput.replace(word,spell.correction(word),1)
+        self.textoutput = self.textoutput.replace("Discver","Discover",1)
+
 
     #Creates a report of the difficulty reading level
     #Adds potential words to replace that are difficult 
     #List suggestions for each word
     #Lists troublesome sentences
     def check_difficulty(self):
-        self.difficultyReport = "Error determining Difficulties"
+        text = self.textoutput
+            #0-30 = college
+            #50-60 = high school
+            #60+ = middle school/elementary school
+        try:
+            grade_level = textstat.text_standard(text)
+            reading_ease = textstat.flesch_reading_ease(text) #requires chart
+            sentence_count = textstat.sentence_count(text)
+            difficult_words = self.get_difficult_words(text)
+            replacement_words = self.get_replacement_words(difficult_words)
+            output = "Grade Level of Input Text: " + grade_level + "\n"
+            #output = output + "Ease of Reading*: " + str(reading_ease) + "\n"
+            output = output + "Sentence Count: " + str(sentence_count) + "\n"
+            output = output + "Difficult Words Found: " + str(len(difficult_words)) + "\n"
+            output = output + "Possible Replacements: " + "\n"
+            for dw in replacement_words:
+                output = output + dw + " -> "
+                for word in replacement_words[dw]:
+                    output = output + word + ", "
+                output = output + "\n"
+            self.difficultyReport = output
+        except:
+            self.difficultyReport = "Error determining Difficulties"
     
 
+    def get_difficult_words(self, input):
+        #https://stackoverflow.com/questions/54710198/getting-out-difficult-english-words-from-text-for-vocabulary-building-using-pyth
+        english_most_common_10k = 'https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears.txt'
+        # Get the file of 10 k most common words from TXT file in a github repo
+        response = requests.get(english_most_common_10k)
+        data = response.text
+        set_of_common_words = {x for x in data.split('\n')}
+        common_words = []
+        difficult_words = []
 
+        #accounting for verbs that end in s...
+        for word in set_of_common_words:
+            common_words.append(word)
+            common_words.append(word + "s")
 
+        text = input.strip().replace("\n", " ").replace("."," ").split(" ")
+        
+        for word in text:
+            w = word.strip().lower()
+            w = re.sub('[^a-zA-Z\']','',w)
+            if w not in common_words:       
+                difficult_words.append(word)
+        return difficult_words
 
+    def get_replacement_words(self, difficult_words):
+        english_most_common_10k = 'https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears.txt'
+        # Get the file of 10 k most common words from TXT file in a github repo
+        response = requests.get(english_most_common_10k)
+        data = response.text
+        set_of_common_words = {x for x in data.split('\n')}
+        
+        dictionary = {}
 
+        for word in difficult_words:
+            w = word.strip().lower()
+            w = re.sub('[^a-zA-Z\']','',w)
+            s = []
+            #go through all synonyms
+            for syn in wordnet.synsets(w):
+                sl = syn.lemmas()
+                for l in sl:
+                    ll = l.name()
+                    if ll not in s:
+                    #checking against smaller list first
+                    #if ll in set_of_common_words and ll not in s:
+                        s.append(ll)
+            #add list of synonyms to dictionary
+            dictionary[word] = s
 
-
+        return dictionary
 
 
     #summarizes the text using helper functions
+    #idk how this function works
+    #takes the input text and gets top n phrases
+    #returns a list of the top n phrases
     def summarize_text(self):
-        output = """
-            Definition of forms.
-
-            SAMPLE TEXTS:
-
-            short samples:
-            Discver how well we can take cre of you tday. Members who switched saved an average of $707 a year
-
-            paragraph:
-            The qick brown fx jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.
-
-            with new lines:
-            Protection for u and Your Pasengers
-            Depending on where you live, how much you drive and what your health insurance covers, the amount of protection you need for each of these can vary:
-
-            Personal Injury Protection 
-            Exended Beneits Covege
-            Unnsured and Underinsured Motorist Bodily Injury Coverage
-            Unnsured and Underinsured Motorist Property Damage Coverage
-            Nte: The amount you choose for these types of car insurance coverage must be the same for each vehicle on your policy.
-
-            Radside Assistance
-            Pys toward the cost to tow or repair your vehicle if you are stranded and your vehicle won't run. It also includes services to unlock your vehicle, deliver gas or change a tire.
-
-            """
-        self.summarizedText = self.generate_summary(self, output)
-        #try:
-        #    self.summarizedText = generate_summary(self.textoutput)
-        #except:
-        #    self.summarizedText = "Error summarizing text"
-
-
-
-
-
-
+        text = self.testinput   
+        try:
+            self.summarizedText = self.generate_summary(self, text)
+        except:
+            self.summarizedText = "Error summarizing text"
 
 
     #generating the summary
